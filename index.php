4646c6788d1dd2390ec8b312da4b1715f2963654
Josua Grawitter <josh@greyage.org>
2009-12-09 23:16:18 +0100

    Added some example code to index.php
    	Show howto properly use html.inc.php
    	not that it provides anything yet...
    
    Signed-off-by: Josua Grawitter <josh@greyage.org>

--- index.php
+++ index.php
@@ -6,6 +6,13 @@ require_once('db.inc.php');
 session_start();
 
 $db = new db();
+/*
+example code to get and display a page from html.inc.php:
 
+$source = new ovp_table_public($db);
+$page = new ovp_page($source);
+$html = $page->get_html();
+echo $html;
 
+*/
 ?>
