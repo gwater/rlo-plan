b30457b5e78f0bff14de6bf74d774c22aa2c26ff
Josua Grawitter <josh@greyage.org>
2009-12-09 22:56:45 +0100

    All classes in html.inc.php are now prototyped
    	they just don't provide any content yet ;-)
    	time for database-foo...
    
    Signed-off-by: Josua Grawitter <josh@greyage.org>

--- html.inc.php
+++ html.inc.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This is the basic API for all content provided by rlo-plan
+ *
+ * There are several different sources which provide the different views each
+ * party is allowed to see. They can either be included directly into an
+ * existing page or pushed through the ovp_page wrapper to create a complete
+ * html page.
+ */
 abstract class ovp_source {
     private $type;
     private $db;
@@ -17,13 +25,17 @@ abstract class ovp_source {
     }
 }
 
+/**
+ * This source provides the public view for students
+ * Sensitive information like teachers names is not included.
+ */
 class ovp_table_public extends ovp_source {
-    public function __construct() {
-        parent::__construct("public");
+    public function __construct($db) {
+        parent::__construct("public", $db);
     }
 
     public function get_header() {
-        $header = "<title>RLO Onlinevertretungsplan</title>";
+        $header = "<title>RLO Onlinevertretungsplan</title>\n";
         return $header;
     }
 
@@ -36,15 +48,126 @@ class ovp_table_public extends ovp_source {
 
 }
 
-class ovp_page {
+/**
+ * This source provides the traditional view for printout.
+ * since it contains sensitive information its access must be restricted to
+ * school personnel.
+ */
+class ovp_table_print extends ovp_source {
+    public function __construct($db) {
+        parent::__construct("print", $db);
+    }
 
+    public function get_header() {
+        $header = "<title>RLO Offlinevertretungsplan</title>\n";
+        return $header;
+    }
+
+    public function get_view() {
+    /* TODO: At this point I need a working database connection
+     * and knowledge of the database layout.
+     */
+
+   }
+
+}
+
+/**
+ * This source provides the view for Frau Lange. It allows adding, removing
+ * and editing entries of the plan and thus access must be restricted to
+ * authorized school personnel.
+ */
+class ovp_lange extends ovp_source {
+    public function __construct($db) {
+        parent::__construct("lange", $db);
+    }
+
+    public function get_header() {
+        $header = "<title>RLO Onlinevertretungsplan Kontrolle</title>\n";
+        return $header;
+    }
+
+    public function get_view() {
+    /* TODO: At this point I need a working database connection
+     * and knowledge of the database layout.
+     */
+
+   }
+
+}
+
+/**
+ * This source provides a simple login interface to authenticate any access
+ * to restricted views.
+ * Naturally access is not restricted.
+ */
+class ovp_login extends ovp_source {
+    public function __construct($db) {
+        parent::__construct("login", $db);
+    }
+
+    public function get_header() {
+        $header = "<title>RLO Onlinevertretungsplan Login</title>\n";
+        return $header;
+    }
+
+    public function get_view() {
+    /* TODO: At this point I need a working database connection
+     * and knowledge of the database layout.
+     */
+
+   }
+
+}
+
+/**
+ * This source provides a simple administration interface which most
+ * importantly allows setting the passwords of any user.
+ * Access msut thus be seriously restricted.
+ */
+class ovp_admin extends ovp_source {
+    public function __construct($db) {
+        parent::__construct("admin", $db);
+    }
+
+    public function get_header() {
+        $header = "<title>RLO Onlinevertretungsplan Administration</title>\n";
+        return $header;
+    }
+
+    public function get_view() {
+    /* TODO: At this point I need a working database connection
+     * and knowledge of the database layout.
+     */
+
+   }
+
+}
+
+/**
+ * This class acts as a wrapper around any ovp_source object and provides
+ * complete html pages based on the content provided by the source.
+ *
+ * Its use is optional and merely provided for convenience and simple setups.
+ */
+class ovp_page {
     private $source; // the ovp_source object used to generate the page
     private $content; // the whole html page
 
-
     public function __construct($source) {
         $this->source = $source;
-        $this->content = "<html></html>";
+        $this->content = generate_html();
+    }
+
+    private function generate_html() {
+        $html = "<!doctype html>\n"
+                ."<html>\n<head>\n"
+                .$source->get_header()
+                ."</head>\n<body>\n"
+                .$source->get_view()
+                ."</body>\n</html>\n";
+
+        return $html;
     }
 
     public function get_html() {
@@ -52,10 +175,4 @@ class ovp_page {
     }
 }
 
-/* missing classes:
- * - ovp_table_lange
- * - ovp_table_print
- * - ovp_admin
- * - ovp_login
- */
 ?>
