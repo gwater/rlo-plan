ac562e367916dea359fda90407e329b0336a13b4
Josua Grawitter <josh@greyage.org>
2009-12-31 16:28:19 +0100

    Remove TODO for SKIP_WEEKENDS
    Signed-off-by: Josua Grawitter <josh@greyage.org>

--- config.inc.php
+++ config.inc.php
@@ -12,7 +12,7 @@ define('DB_PASS', ''); // database password
 
 define('DELETE_OLDER_THAN', -1);       // positive: entries older than DELETE_OLDER_THAN days are automatically deleted;
                                        // negative: never delete anything
-define('SKIP_WEEKENDS', true);         // true: weekends are skipped when calculating the age of an entry, (TODO)
+define('SKIP_WEEKENDS', true);         // true: weekends are skipped when calculating the age of an entry,
                                        //       e.g. it's Monday, entry is from Friday, DELETE_OLDER_THAN = 1 --> entry is _not_ deleted;
                                        // false: weekends are just like every other day (this would suck for obvious reasons)
 define('PRIV_DEFAULT', 1);             // default privilege level for logged out visitors, ovp_logger::PRIV_DEFAULT
