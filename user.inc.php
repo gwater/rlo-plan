88df0a2f52de561f0a9b25c9db5cc00aafd1b7ac
Josua Grawitter <josh@greyage.org>
2010-01-05 16:48:45 +0100

    Better message for privilege manipulation denail
    Signed-off-by: Josua Grawitter <josh@greyage.org>

--- user.inc.php
+++ user.inc.php
@@ -157,7 +157,7 @@ class ovp_user extends ovp_asset {
     public function set_privilege($newpriv) {
         $admin = ovp_logger::get_current_user($this->db);
         if ($admin->get_id() == $this->id) {
-            ovp_msg::fail('Eigener Account darf nicht gelöscht werden');
+            ovp_msg::fail('Eigener Account darf nicht degradiert werden');
         }
         foreach (self::$roles as $priv => $role) {
             if ($newpriv == $priv) {
