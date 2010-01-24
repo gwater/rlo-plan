e02a2fdc1bef6f71e5a152219015554c33be3805
Tillmann Karras <tilkax@gmail.com>
2010-01-24 05:45:02 +0100

    Enable in-place update of source.zip
    (open wizard.php to trigger repack of source.zip)

--- interfaces.inc.php
+++ interfaces.inc.php
@@ -33,10 +33,13 @@ class ovp_zipper {
             return false;
         }
         foreach ($files as $file) {
-            $zip->addFile($file);
+            if ($file == 'config.inc.php') {
+                $zip->addFromString('config.inc.php', ovp_config::get_clean_copy());
+            } else {
+                $zip->addFile($file);
+            }
         }
-        $zip->close();
-        return true;
+        return $zip->close();
     }
 
     private static function list_files($dir) {
@@ -81,6 +84,30 @@ class ovp_config {
         } else {
             ovp_msg::fail('Ändern der Konfigurationsdatei gescheitert');
         }
+    }
+
+    public static function get_clean_copy() {
+        return "<?php
+
+define('FIRST_RUN', true);            // this value is automatically set to false on first run
+define('DEBUG', true);                 // true:  detailed error messages are shown
+                                       // false: only very short or no error messages are shown
+                                       // set this to false when uploading to IRL server!
+
+define('DB_HOST', 'localhost');        // database server address
+define('DB_BASE', '');         // database name
+define('DB_USER', '');         // database user name
+define('DB_PASS', ''); // database password
+
+define('DELETE_OLDER_THAN', -1);       // positive: entries older than DELETE_OLDER_THAN days are automatically deleted;
+                                       // negative: never delete anything
+define('SKIP_WEEKENDS', true);         // true: weekends are skipped when calculating the age of an entry,
+                                       //       e.g. it's Monday, entry is from Friday, DELETE_OLDER_THAN = 1 --> entry is _not_ deleted;
+                                       // false: weekends are just like every other day (this would suck for obvious reasons)
+define('PRIV_DEFAULT', 1);             // default privilege level for logged out visitors, ovp_logger::PRIV_DEFAULT
+
+?>
+";
     }
 }
 
